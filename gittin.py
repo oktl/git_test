4926aeefc5f6d03c415c3d7519e2e4cd8f40ae29
oktl <ttim.lang@gmail.com>
2023-09-22 14:46:42 -0600

    1st Commit

---
+++ gittin.py
@@ -0,0 +1,14 @@
+# Test file for practicing git and git-hub in VS Code.
+
+import socket
+import os
+import pprint as pprint
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+print(f'\nHostname is {hostname}')
+print(f'\nIP address is {ip_address}')
+
+path = os.environ.get("PATH")
+print(f'\nPath is: {path}')
